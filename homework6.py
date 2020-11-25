792e7215e7dea98282087bdc0e019a7e2aebbb47
Nurlan <nur23kg@mail.com>
2020-11-25 20:16:37 +0600

    Validation

---
+++ homework6.py
@@ -0,0 +1,9 @@
+login = input('Enter login: ')
+password = input('Enter password: ')
+
+print('Login and password saved')
+
+new_password = input('Change the password: ')
+password = new_password
+
+print('password changed')
