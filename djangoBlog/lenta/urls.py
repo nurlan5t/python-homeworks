97ef35f7da6f40ef6253cbfa409e98d223e77b99
Nurlan <nur23kg@mail.com>
2021-01-27 21:42:42 +0600

    Django delete

--- djangoBlog/lenta/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.show_lenta),
-    path('like-add/', views.add_like),
-    path('comment-add/', views.add_comment)
-]
